1811627630bde6ed4c2f48280ef84c8d9871a473
Luke Davis <luke@finesse.host>
2024-10-05 22:28:07 -0500

    clean nx script up

--- src/utils/nx.sh
+++ src/utils/nx.sh
@@ -10,12 +10,12 @@ nx() {
 
 check_dependencies() {
     if ! check_cmd node; then
-        echo >&2 "Nx requires NodeJS to be available. To install NodeJS and NPM, see: https://nodejs.org/en/download"
+        echo >&2 "nx requires NodeJS to be available. To install NodeJS and NPM, see: https://nodejs.org/en/download"
         exit 1
     fi
 
     if ! check_cmd npm; then
-        echo >&2 "Nx requires npm to be available. To install NodeJS and NPM, see: https://nodejs.org/en/download/ ."
+        echo >&2 "nx requires npm to be available. To install NodeJS and NPM, see: https://nodejs.org/en/download"
         exit 1
     fi
 }
